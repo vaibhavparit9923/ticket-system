4d4629ff12b011defd7164233a685db01fe47537
Vaibhav Parit <vaibhavbparit@gmail.com>
2026-04-01 01:09:23 +0530

    frontend ready for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 // 🔥 Backend base URL
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-});
+const BASE_URL = "https://ticket-system-8zeq.onrender.com";
 
+export const API = {
+  login: `${BASE_URL}/login`,
+  register: `${BASE_URL}/register`,
+  events: `${BASE_URL}/events`,
+  book: `${BASE_URL}/book`,
+};
 // 🔐 Token automatically attach
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
